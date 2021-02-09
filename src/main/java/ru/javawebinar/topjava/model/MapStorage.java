e286dd073237e656cea22bf724a8466f0dfe9b3f
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2021-02-09 11:40:03 +0900

    Change HashMap to ConcurrentHashMap

--- src/main/java/ru/javawebinar/topjava/model/MapStorage.java
+++ src/main/java/ru/javawebinar/topjava/model/MapStorage.java
@@ -1,12 +1,12 @@
 package ru.javawebinar.topjava.model;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class MapStorage implements Storage {
-    Map<String, Meal> meals = new HashMap<>();
+    Map<String, Meal> meals = new ConcurrentHashMap<>();
 
     @Override
     public void create(Meal meal) {
