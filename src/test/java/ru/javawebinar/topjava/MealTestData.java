86f403f760761fd048de4d06b4ff77e0aeddce47
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2021-02-24 13:09:50 +0900

    Add MealServiceTest

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -1,32 +1,33 @@
 package ru.javawebinar.topjava;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
 
 public class MealTestData {
     public static final int NOT_FOUND = 10;
+    public static final int FIRST_MEAL = START_SEQ+2;
 
-    public static final Meal meal11 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
-    public static final Meal meal12 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000);
-    public static final Meal meal13 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500);
-    public static final Meal meal14 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100);
-    public static final Meal meal15 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000);
-    public static final Meal meal16 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500);
-    public static final Meal meal17 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410);
+    public static final Meal meal11 = new Meal(FIRST_MEAL, LocalDateTime.of(2021, Month.FEBRUARY, 10, 10, 0), "Завтрак", 500);
+    public static final Meal meal12 = new Meal(START_SEQ+3, LocalDateTime.of(2021, Month.FEBRUARY, 10, 13, 0), "Обед", 1000);
+    public static final Meal meal13 = new Meal(START_SEQ+4, LocalDateTime.of(2021, Month.FEBRUARY, 10, 20, 0), "Ужин", 500);
+    public static final Meal meal14 = new Meal(START_SEQ+5, LocalDateTime.of(2021, Month.FEBRUARY, 11, 0, 0), "Граница", 100);
+    public static final Meal meal15 = new Meal(START_SEQ+6, LocalDateTime.of(2021, Month.FEBRUARY, 11, 10, 0), "Завтрак", 500);
+    public static final Meal meal16 = new Meal(START_SEQ+7, LocalDateTime.of(2021, Month.FEBRUARY, 11, 13, 0), "Обед", 1000);
+    public static final Meal meal17 = new Meal(START_SEQ+8, LocalDateTime.of(2021, Month.FEBRUARY, 11, 20, 0), "Ужин", 410);
 
-    public static final Meal meal21 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 10, 0), "Завтрак", 500);
-    public static final Meal meal22 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 13, 0), "Обед", 1000);
-    public static final Meal meal23 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 30, 20, 0), "Ужин", 500);
-    public static final Meal meal24 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 0, 0), "Еда на граничное значение", 100);
-    public static final Meal meal25 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000);
-    public static final Meal meal26 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500);
-    public static final Meal meal27 = new Meal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410);
+    public static final Meal meal21 = new Meal(START_SEQ+9, LocalDateTime.of(2020, Month.FEBRUARY, 10, 10, 0), "Завтрак", 500);
+    public static final Meal meal22 = new Meal(START_SEQ+10, LocalDateTime.of(2020, Month.FEBRUARY, 10, 13, 0), "Обед", 1000);
+    public static final Meal meal23 = new Meal(START_SEQ+11, LocalDateTime.of(2020, Month.FEBRUARY, 10, 20, 0), "Ужин", 500);
+    public static final Meal meal24 = new Meal(START_SEQ+12, LocalDateTime.of(2020, Month.FEBRUARY, 11, 0, 0), "Граница", 100);
+    public static final Meal meal25 = new Meal(START_SEQ+13, LocalDateTime.of(2020, Month.FEBRUARY, 11, 10, 0), "Завтрак", 500);
+    public static final Meal meal26 = new Meal(START_SEQ+14, LocalDateTime.of(2020, Month.FEBRUARY, 11, 13, 0), "Обед", 1000);
+    public static final Meal meal27 = new Meal(START_SEQ+15, LocalDateTime.of(2020, Month.FEBRUARY, 11, 20, 0), "Ужин", 410);
 
     public static Meal getNew() {
         return new Meal(LocalDateTime.of(2021, Month.JANUARY, 31, 23, 0), "Test eat", 777);
@@ -44,11 +45,11 @@ public class MealTestData {
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 
-    public static void assertMatch(Iterable<User> actual, User... expected) {
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
         assertMatch(actual, Arrays.asList(expected));
     }
 
-    public static void assertMatch(Iterable<User> actual, Iterable<User> expected) {
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
         assertThat(actual).usingRecursiveComparison().isEqualTo(expected);
     }
 }
--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -1,32 +1,105 @@
 package ru.javawebinar.topjava.service;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.MealTestData;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.util.exception.NotFoundException;
 
-import static org.junit.Assert.*;
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
 
+import static org.junit.Assert.assertThrows;
+import static ru.javawebinar.topjava.MealTestData.*;
+import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
+import static ru.javawebinar.topjava.UserTestData.USER_ID;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
 
     @Test
     public void get() {
+        Meal meal = service.get(FIRST_MEAL, USER_ID);
+        assertMatch(meal, meal11);
+    }
+
+    @Test
+    public void getNotFound() {
+        assertThrows(NotFoundException.class, () -> service.get(NOT_FOUND, USER_ID));
     }
 
     @Test
     public void delete() {
+        service.delete(FIRST_MEAL, USER_ID);
+        assertThrows(NotFoundException.class, () -> service.get(FIRST_MEAL, USER_ID));
+    }
+
+    @Test
+    public void deleteNotFound() {
+        assertThrows(NotFoundException.class, () -> service.delete(NOT_FOUND, USER_ID));
     }
 
     @Test
     public void getBetweenInclusive() {
+        List<Meal> actual = service.getBetweenInclusive(LocalDate.MIN, LocalDate.of(2021, Month.FEBRUARY, 10), USER_ID);
+        assertMatch(actual, meal13, meal12, meal11);
     }
 
     @Test
     public void getAll() {
+        List<Meal> actual = service.getAll(USER_ID);
+        assertMatch(actual, meal17, meal16, meal15, meal14, meal13, meal12, meal11);
     }
 
     @Test
     public void update() {
+        Meal updated = getUpdated();
+        service.update(updated, USER_ID);
+        assertMatch(service.get(FIRST_MEAL, USER_ID), getUpdated());
+    }
+
+    @Test
+    public void updateMealAnotherUser() {
+        Meal updated = MealTestData.getUpdated();
+        assertThrows(NotFoundException.class, () -> service.update(updated, ADMIN_ID));
     }
 
     @Test
     public void create() {
+        Meal created = service.create(MealTestData.getNew(), USER_ID);
+        Integer newId = created.getId();
+        Meal newMeal = MealTestData.getNew();
+        newMeal.setId(newId);
+        assertMatch(created, newMeal);
+        assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    public void createWithExistingDatetime() {
+        Meal newMeal = new Meal(meal11);
+        newMeal.setId(null);
+        assertThrows(DuplicateKeyException.class, () -> service.create(newMeal, USER_ID));
     }
 }
