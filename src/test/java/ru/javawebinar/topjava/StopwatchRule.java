b28c3a571ac0fe758f27eddff3f4878886c5a894
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2021-03-03 15:07:31 +0900

    Cut useless commemnts from StopwatchRule

--- src/test/java/ru/javawebinar/topjava/StopwatchRule.java
+++ src/test/java/ru/javawebinar/topjava/StopwatchRule.java
@@ -9,7 +9,6 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-
 public class StopwatchRule extends Stopwatch {
     private static final Logger log = LoggerFactory.getLogger(StopwatchRule.class);
     public static final Map<String, Long> results = new LinkedHashMap<>();
@@ -21,23 +20,4 @@ public class StopwatchRule extends Stopwatch {
                 TimeUnit.NANOSECONDS.toMicros(nanos));
         results.put(description.getMethodName(), TimeUnit.NANOSECONDS.toMillis(nanos));
     }
-
-    /*private static final Logger log = LoggerFactory.getLogger(StopwatchRule.class);
-    public static final Map<String, Long> results = new HashMap<>();
-    private long time;
-    private long timeInterval;
-
-    @Override
-    public Statement apply(Statement base, Description description) {
-        return new Statement() {
-            @Override
-            public void evaluate() throws Throwable {
-                time = System.currentTimeMillis();
-                base.evaluate();
-                timeInterval = System.currentTimeMillis() - time;
-                log.debug("Test method: {}() passed in {} ms.", description.getMethodName(), timeInterval);
-                results.put(description.getMethodName() + "()", timeInterval);
-            }
-        };
-    }*/
 }
