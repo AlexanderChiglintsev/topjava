cf221092d5771756a172ffaf101a3ac42902bb2e
Alexander Chiglintsev <alex.chiglintsev@yandex.ru>
2021-03-08 23:55:20 +0900

    Spring profiles for jpa, datajpa, jdbc

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -35,7 +35,10 @@ import static ru.javawebinar.topjava.UserTestData.USER_ID;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = Profiles.ActiveDbProfileResolver.class)
+@ActiveProfiles({
+        Profiles.DATAJPA,
+        Profiles.POSTGRES_DB
+})
 public class MealServiceTest {
     private static final Logger log = getLogger("result");
 
--- src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/UserServiceTest.java
@@ -28,7 +28,11 @@ import static ru.javawebinar.topjava.UserTestData.*;
 })
 @RunWith(SpringRunner.class)
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles(resolver = Profiles.ActiveDbProfileResolver.class)
+//(resolver = Profiles.ActiveDbProfileResolver.class)
+@ActiveProfiles({
+        Profiles.DATAJPA,
+        Profiles.POSTGRES_DB
+})
 public class UserServiceTest {
 
     @Autowired
